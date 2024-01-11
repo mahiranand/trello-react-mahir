134d3cdc222916bd27f831d2b83b35bbbd035531
mahiranand <mahiranand08@gmail.com>
2024-01-11 15:14:42 +0530

    checklist add delete feature added

--- src/components/CheckList.jsx
+++ src/components/CheckList.jsx
@@ -1,7 +1,10 @@
-import { Box, Button, Modal, Typography } from "@mui/material";
+import { Box, Button, Modal, Popover, TextField } from "@mui/material";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import AddIcon from "@mui/icons-material/Add";
+import CloseIcon from "@mui/icons-material/Close";
+import CheckBoxOutlinedIcon from "@mui/icons-material/CheckBoxOutlined";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 const yourKey = "e329af9483b37135d074e667f5f48020";
 const yourToken =
@@ -22,6 +25,8 @@ const style = {
 // eslint-disable-next-line react/prop-types
 const CheckList = ({ cardId, name, open, handleClose }) => {
   const [checklistData, setChecklistData] = useState([]);
+  const [displayTextArea, setDisplayTextArea] = useState(null);
+  const [checkListName, setCheckListName] = useState("");
 
   useEffect(() => {
     axios
@@ -34,17 +39,36 @@ const CheckList = ({ cardId, name, open, handleClose }) => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  const addCheckList = (cId) => {
+  const addCheckList = () => {
     axios
       .post(
-        `https://api.trello.com/1/cards/${cId}/checklists?name=mahir&key=${yourKey}&token=${yourToken}`
+        `https://api.trello.com/1/cards/${cardId}/checklists?name=${checkListName}&key=${yourKey}&token=${yourToken}`
       )
       .then((res) => {
         setChecklistData([...checklistData, res.data]);
       });
   };
 
-  console.log(checklistData);
+  const deleteCheckList = (cListId) => {
+    axios
+      .delete(
+        `https://api.trello.com/1/cards/${cardId}/checklists/${cListId}?key=${yourKey}&token=${yourToken}`
+      )
+      .then(() => {
+        setChecklistData(checklistData.filter(({ id }) => id !== cListId));
+      });
+  };
+
+  const handleAddBtnClose = () => {
+    setDisplayTextArea(null);
+  };
+
+  const handleAddBtnOpen = (e) => {
+    setDisplayTextArea(e.currentTarget);
+  };
+
+  const displayBtnId = displayTextArea ? "simple-popover" : undefined;
+
   return (
     <Modal
       open={open}
@@ -53,18 +77,25 @@ const CheckList = ({ cardId, name, open, handleClose }) => {
       aria-describedby="modal-modal-description"
     >
       <Box sx={style}>
-        <Typography
-          sx={{
-            fontSize: "1.5rem",
-            backgroundColor: "blanchedalmond",
-            borderRadius: "0.5rem",
-          }}
-          id="modal-modal-title"
-          variant="h6"
-          component="h2"
-        >
-          Card Name: {name}
-        </Typography>
+        <div style={{ display: "flex", justifyContent: "space-between" }}>
+          <span
+            style={{
+              fontSize: "1.5rem",
+              backgroundColor: "blanchedalmond",
+              borderRadius: "0.5rem",
+              padding: "0.5rem 1rem",
+            }}
+            id="modal-modal-title"
+          >
+            Card Name: {name}
+          </span>
+          <Button
+            sx={{ backgroundColor: "blanchedalmond" }}
+            onClick={handleClose}
+          >
+            <CloseIcon sx={{ color: "black" }} />
+          </Button>
+        </div>
         <div
           style={{
             display: "flex",
@@ -75,28 +106,91 @@ const CheckList = ({ cardId, name, open, handleClose }) => {
         >
           <p style={{ fontSize: "1.5rem" }}>CheckLists</p>
           <Button
+            aria-describedby={displayBtnId}
             variant="contained"
             size="small"
             endIcon={<AddIcon />}
-            onClick={() => {
-              addCheckList(cardId);
-            }}
+            onClick={handleAddBtnOpen}
           >
             Add
           </Button>
+          <Popover
+            id={displayBtnId}
+            open={displayTextArea ? true : false}
+            anchorEl={displayTextArea}
+            onClose={handleAddBtnClose}
+            anchorOrigin={{
+              vertical: "bottom",
+              horizontal: "left",
+            }}
+          >
+            {" "}
+            <div
+              style={{
+                padding: "1rem",
+                display: "flex",
+                flexDirection: "column",
+              }}
+            >
+              <TextField
+                value={checkListName}
+                id="outlined-basic"
+                label="CheckList Name"
+                variant="outlined"
+                size="small"
+                sx={{ paddingBottom: "0.5rem" }}
+                onChange={(e) => setCheckListName(e.target.value)}
+              />
+              <Button
+                sx={{ color: "black", backgroundColor: "lightsteelblue" }}
+                disabled={checkListName ? false : true}
+                onClick={() => {
+                  addCheckList();
+                  setCheckListName("");
+                }}
+              >
+                Add
+              </Button>
+            </div>
+          </Popover>
         </div>
         <div
           style={{
-            marginTop: "1rem",
+            marginTop: "1.5rem",
             display: "flex",
             flexDirection: "column",
-            gap: "0.5rem",
+            gap: "0.7rem",
           }}
         >
-          {checklistData.map(({ name, id }, idx) => (
-            <Typography key={id}>
-              {idx + 1}: {name}
-            </Typography>
+          {checklistData.map(({ name, id }) => (
+            <div
+              key={id}
+              style={{
+                display: "flex",
+                alignItems: "center",
+                width: "100%",
+                justifyContent: "space-between",
+              }}
+            >
+              <div style={{ display: "flex", gap: "0.5rem" }}>
+                <CheckBoxOutlinedIcon />
+                <p style={{ fontSize: "1.2rem" }}>{name}</p>
+              </div>
+              <Button
+                size="small"
+                variant="outlined"
+                startIcon={<DeleteIcon />}
+                sx={{
+                  color: "red",
+                  border: "1px solid red",
+                }}
+                onClick={() => {
+                  deleteCheckList(id);
+                }}
+              >
+                Delete
+              </Button>
+            </div>
           ))}
         </div>
       </Box>
